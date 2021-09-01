fad06d837aadba70965fc598f0ee6dd67876dbba
redrambo9 <89056787+redrambo9@users.noreply.github.com>
2021-09-01 17:21:30 +0530

    Add files via upload

---
+++ src/AssistedAssignments/src/Assisted/RegularExpressions.java
@@ -0,0 +1,75 @@
+package Assisted;
+import java.util.regex.*;
+
+public class RegularExpressions {
+
+	public static void main(String[] args) {
+
+		String pattern = "[a-z]+";
+		String check = "Regular Expressions";
+		Pattern p = Pattern.compile(pattern);
+		Matcher c = p.matcher(check);
+		
+		while (c.find())
+	      	System.out.println( check.substring( c.start(), c.end() ) );
+		
+		// other examples of regex
+		System.out.println (Pattern.matches("geeksforge*ks", "geeksforgeeks")); 
+		  
+        // Following line prints "false" because the whole text "geeksfor" doesn't match pattern "g*geeks*" 
+        System.out.println (Pattern.matches("g*geeks*", "geeksfor")); 
+        
+        
+        //------------------------------------PATTERN SEARCHING------------------------------------//
+        System.out.println(Pattern.matches(".s", "as"));//true (2nd char is s)  
+        System.out.println(Pattern.matches(".s", "mk"));//false (2nd char is not s) 
+        
+        System.out.println(Pattern.matches("[amn]", "abcd"));//false (not a or m or n)  
+        System.out.println(Pattern.matches("[amn]", "a"));//true (among a or m or n) 
+        
+        
+        //------------------------------------QUANTIFIER------------------------------------------//
+        System.out.println("? quantifier ....");  
+        System.out.println(Pattern.matches("[amn]?", "a"));//true (a or m or n comes one time)  
+        System.out.println(Pattern.matches("[amn]?", "aaa"));//false (a comes more than one time)  
+        System.out.println(Pattern.matches("[amn]?", "aammmnn"));//false (a m and n comes more than one time)  
+        System.out.println(Pattern.matches("[amn]?", "aazzta"));//false (a comes more than one time)  
+        System.out.println(Pattern.matches("[amn]?", "am"));//false (a or m or n must come one time)  
+          
+        System.out.println("+ quantifier ....");  
+        System.out.println(Pattern.matches("[amn]+", "a"));//true (a or m or n once or more times)  
+        System.out.println(Pattern.matches("[amn]+", "aaa"));//true (a comes more than one time)  
+        System.out.println(Pattern.matches("[amn]+", "aammmnn"));//true (a or m or n comes more than once)  
+        System.out.println(Pattern.matches("[amn]+", "aazzta"));//false (z and t are not matching pattern)  
+          
+        System.out.println("* quantifier ....");  
+        System.out.println(Pattern.matches("[amn]*", "ammmna"));//true (a or m or n may come zero or more times)  
+         
+               
+        //------------------------------------META-CHARACTER------------------------------------------// 
+        System.out.println(Pattern.matches("\\d", "abc"));//false (non-digit)  
+        System.out.println(Pattern.matches("\\d", "1"));//true (digit and comes once)  
+        System.out.println(Pattern.matches("\\d", "4443"));//false (digit but comes more than once)  
+        System.out.println(Pattern.matches("\\d", "323abc"));//false (digit and char)  
+          
+        System.out.println("metacharacters D....");  //D means non-digit  
+          
+        System.out.println(Pattern.matches("\\D", "abc"));//false (non-digit but comes more than once)  
+        System.out.println(Pattern.matches("\\D", "1"));//false (digit)  
+        System.out.println(Pattern.matches("\\D", "4443"));//false (digit)  
+        System.out.println(Pattern.matches("\\D", "323abc"));//false (digit and char)  
+        System.out.println(Pattern.matches("\\D", "m"));//true (non-digit and comes once)  
+          
+        System.out.println("metacharacters D with quantifier....");  
+        System.out.println(Pattern.matches("\\D*", "mak"));//true (non-digit and may come 0 or more times)  
+        
+        //System.out.println(Pattern.matches("[A-Z]+\\d{3}[a-zA-Z]*", "S564teutidkghdjdkg"));
+  
+		}
+}
+
+
+
+
+
+
